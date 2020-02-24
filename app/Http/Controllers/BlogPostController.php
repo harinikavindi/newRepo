d908671c90ebdf33af75a634ecc80d4010afba72
dev737 <61407199+dev737@users.noreply.github.com>
2020-02-24 04:33:47 -0800

    sxkxjsksjas

--- app/Http/Controllers/BlogPostController.php
+++ app/Http/Controllers/BlogPostController.php
@@ -28,29 +28,67 @@ class BlogPostController extends Controller
         //dd('hjhjhjdhja');
 
 
-        // $post =(BlogPost::all());
-        // return response(['post'=>$post]);            // display the table content
+        $post =(BlogPost::all());
+        return response(['post'=>$post]);            // display the table content
 
        
-
 
 
     //
     }
 
-    /**
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \
+    
         //
     }
 
-    newmethod(){
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $post = new BlogPost([
+            'title' => $request->get('title'),
+            'description' => $request->get('description')
+          ]);
+    
+          $post->save();
+    
+          return response()->json('success');
+        //
+    }
 
-kkkkkk;
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\BlogPost  $blogPost
+     * @return \Illuminate\Http\Response
+     */
+    public function show(BlogPost $blogPost)
+    {
+        //
+    }
 
-}
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\BlogPost  $blogPost
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        // $post = BlogPost::find($id);
+        // return response()->json($post);//     edit the blog postst
+
+
+        // $data = AgeGroup::find(1);                 // calling the relationship made for categoryAgeGroup- show 1st ID 
+        // dd($data);
+
+        $data = CategoryAgeGroup::where(['category_id'=>'1'])->get();                // calling the relationship made for categoryAgeGroup- show 1st ID 
+        dd($data);
+    }
 
     /**
      * Update the specified resource in storage.
@@ -62,7 +100,16 @@ kkkkkk;
     public function update(Request $request, $id)
     {
         $post = BlogPost::find($id);
-        $
+        $post->update($request->all());
+        return response()->json('successfully updated');//
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\BlogPost  $blogPost
+     * @return \Illuminate\Http\Response
+     */
     public function delete($id)
     {
         $post = BlogPost::find($id);
